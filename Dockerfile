2de7f8878d4b98e0fa9b0a5eec3a27fc45ca01c9
Austin Burger <aburger91@gmail.com>
2020-08-07 20:50:30 -0500

    prep for github

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:12-alpine
-WORKDIR /yell
-COPY . .
-RUN npm i
-EXPOSE 4242
-ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-psql -U postgres
-yellapppassword
-create user yellapp with password 'yellapppassword' createdb;
-\q
-npx sequelize-cli db:create
-npx sequelize-cli db:migrate
-
-npm run dev
